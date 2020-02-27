948bb4c91452e65310dbeacda501f833c4b6b4f0
Keith Policano <kpolicano@gmail.com>
2020-02-27 08:00:19 -0600

    adding packaging

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
 plugins {
   `java-library`
   scala
+  `maven-publish`
 }
+
+group = "com.linearframework"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
   jcenter()
@@ -20,4 +24,23 @@ dependencies {
 tasks.named<Jar>("jar") {
   from(sourceSets["main"].output)
   from(sourceSets["main"].allSource)
+}
+
+publishing {
+  repositories {
+    maven {
+      name = "GithubPackages"
+      url = uri("https://maven.pkg.github.com/linear-framework/linear-macros")
+      credentials {
+        username = System.getenv("GITHUB_USER")
+        password = System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+  publications {
+    create<MavenPublication>("PublishToGithub") {
+      artifactId = "macros"
+      from(components["java"])
+    }
+  }
 }
